d4bd9b1f4acb59ed2949fb380f5241e3613f60ff
yuruOvO <30980083+yururyuru00@users.noreply.github.com>
2020-07-30 20:49:07 +0900

    Add files via upload

--- data/utilities/data_plot.py
+++ data/utilities/data_plot.py
@@ -26,42 +26,38 @@ class BHTSNE(sklearn.base.BaseEstimator, sklearn.base.TransformerMixin):
             rand_seed=self.rand_seed
         )
 
-def plot_feature(data, label, *, savename='feature.png'):
+def t_sne(data):
     bh = BHTSNE(dimensions=2, perplexity=30.0, theta=0.5, rand_seed=-1, max_iter=10000)
     tsne = bh.fit_transform(data)
+    
+    return tsne
 
-    xmin = tsne[:,0].min()
-    xmax = tsne[:,0].max()
-    ymin = tsne[:,1].min()
-    ymax = tsne[:,1].max()
+def plot_feature(data_path, label_path, *, savename='./feature.png'):
+    label =  np.load(label_path)
+    data = np.load(data_path)
+    x = t_sne(data)
+
+    xmin = x[:,0].min()
+    xmax = x[:,0].max()
+    ymin = x[:,1].min()
+    ymax = x[:,1].max()
 
     plt.figure( figsize=(16,12) )
-    plt.scatter(tsne[:,0], tsne[:,1], c=label)
+    plt.scatter(x[:,0], x[:,1], c=label)
     plt.axis([xmin,xmax,ymin,ymax])
     plt.xlabel("component 0")
     plt.ylabel("component 1")
     plt.title("data visualization")
-    plt.savefig("../experiment/" + savename + '.png')
+    plt.savefig(savename + '.png')
 
-def  plot_label(**kwargs):
-    min__, max__ = 100, -1
-    for pred in kwargs.values():
-        min_, max_ = np.min(pred), np.max(pred)
-        if(min_ < min__):
-            min__ = min_
-        if(max_ > max__):
-            max__ = max_
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_path', type=str, 
+                                    help='data_path ubtil data')
+    parser.add_argument('label_path', type=str, 
+                                    help='label_path ubtil label')
+    parser.add_argument('--save', type=str, default='test',
+                                    help='filename when save')
 
-    fig = plt.figure(figsize=(35, 17))
-    ax = [fig.add_subplot(1, 3, 1),fig.add_subplot(1, 3, 2),fig.add_subplot(1, 3, 3)]
-    for i, key in enumerate(kwargs.keys()):
-        preds = kwargs[key][::100]
-        for pred in preds:
-            ax[i].plot([i for i in range(len(pred))], pred)
-        ax[i].set_ylim(min__, max__)
-    plt.savefig('./pred_label.png')
-
-label = np.loadtxt('../experiment/DANElabel.csv')
-for epoch in [0,1,2,49,50,51,52]:
-    Zn = np.loadtxt('../experiment/epochs200_skips50/Zn_epoch#{}.csv'.format(epoch))
-    plot_feature(Zn, label, savename='epochs200_skips50/Zn_epoch{}_label'.format(epoch))
+    args = parser.parse_args()
+    plot_feature(args.data_path, args.label_path, savename=args.save)
