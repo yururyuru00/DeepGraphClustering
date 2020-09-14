86a5b9a94cee3001f68e91e9815aa63d469d179d
yuruOvO <30980083+yururyuru00@users.noreply.github.com>
2020-09-14 14:25:27 +0900

    Add files via upload

--- layers.py
+++ layers.py
@@ -1,7 +1,5 @@
 import math
 import numpy as np
-from numpy import linalg
-
 import torch
 from torch.nn.parameter import Parameter
 from torch.nn.modules.module import Module
@@ -14,7 +12,7 @@ class GraphConvolution(Module):
         self.in_features = in_features
         self.out_features = out_features
         self.weight = Parameter(torch.FloatTensor(in_features, out_features))
-        ##パラメータはnn.parameterオブジェクトとしないと，後で重みの更新ができない
+        # パラメータはnn.parameterオブジェクトとしないと，後で重みの更新ができない
         if bias:
             self.bias = Parameter(torch.FloatTensor(out_features))
         else:
@@ -27,13 +25,14 @@ class GraphConvolution(Module):
         if self.bias is not None:
             self.bias.data.uniform_(-stdv, stdv)
 
-    def forward(self, input, adj): #input:特徴行列Z,    adj:隣接行列Aとして渡される
-        support = torch.mm(input, self.weight) #畳み込み DAZWの内，ZW(=support)の演算
-        output = torch.spmm(adj, support) #畳み込み DAZWの内，(DA)supportの演算
-        if self.bias is not None: #今回はbiasあり
+    def forward(self, input, adj):  # input:特徴行列Z,    adj:隣接行列Aとして渡される
+        support = torch.mm(input, self.weight)  # 畳み込み DAZWの内，ZW(=support)の演算
+        output = torch.spmm(adj, support)  # 畳み込み DAZWの内，(DA)supportの演算
+        if self.bias is not None:  # 今回はbiasあり
             return output + self.bias
         else:
             return output
+
 
 class FrobeniusNorm(Module):
     def __init__(self):
@@ -42,11 +41,12 @@ class FrobeniusNorm(Module):
     def forward(self, inputs, targets):
         diff = inputs - targets
         return torch.norm(diff)
-    
+
+
 class HardClusterLoss(Module):
     def __init__(self):
         super(HardClusterLoss, self).__init__()
 
     def forward(self, inputs):
-        #return rate*torch.norm(inputs) #引数にrateを実装すること
+        # return rate*torch.norm(inputs) #引数にrateを実装すること
         return torch.norm(inputs)
